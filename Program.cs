5d0d4e8e1253af9c65585d619d294b6f1b2ca7e2
tupapimaroyu <maf0033052@est.univalle.edu>
2023-05-10 16:08:24 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@
         {
             Console.WriteLine("hola mi amor buneas tardes");
             int result = sum(2, 3);
-            
+            string Nombrecompleto = nombrecompleto("fernando","marupa ");
 
         }
         public static int sum(int n1, int n2)
         {
             return n1 + n2;
+        }
+        public static string nombrecompleto(string name, string lastname)
+        {
+            return name + lastname;
         }
     }
 }
