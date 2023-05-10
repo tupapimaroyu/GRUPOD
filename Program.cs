1d7d26c001c46fa404dfdb777a53bfb901cf4678
tupapimaroyu <maf0033052@est.univalle.edu>
2023-05-10 16:01:04 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mi amor buneas tardes");
+            int result = sum(2, 3);
+            
+
+        }
+        public static int sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
